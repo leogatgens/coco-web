6056d0aa52eb5f6db5c56a141702466828003eb6
leogatgens <legatgens@gmail.com>
2022-07-18 10:26:43 -0600

    Changed fav.icon and title

--- src/views/App.js
+++ src/views/App.js
@@ -6,10 +6,19 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import {Helmet} from "react-helmet";
 const App = () =>{
 
+  
     return(
+
         <BrowserRouter>    
+         <Helmet>
+                <meta charSet="utf-8" />
+                <meta name='description' content='Testing and learning react.js'/>
+                <title>Home - coco website</title>
+                <link rel="canonical" href="http://localhost:3000/" />
+            </Helmet>
             <Routes>
                 <Route path="/" element={<Home/>}>    
                     <Route
