b84c14ca275d8bf41b02058be5d716b564e8bae3
leogatgens <legatgens@gmail.com>
2022-07-22 12:20:53 -0600

    enable reducx dev tools

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import reducers from './reducers'
 import App from './views/App'
 import thunk from 'redux-thunk';
 //Por alguna razon stric mode muestrar didmount dos veces por tener esto https://andreasheissenberger.medium.com/react-components-render-twice-any-way-to-fix-this-91cf23961625
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(reducers,applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // to reduxtools
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk)));
 root.render(
   // <React.StrictMode>    
     <Provider store={store}>
